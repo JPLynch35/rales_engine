28f2b97c7071f53bf311bb74828e5f1e85d4871d
JP Lynch <JPLynch35@gmail.com>
2018-08-13 21:31:26 -0600

    add request test and functionality for invoices search params

--- spec/requests/api/v1/invoices/search_request_spec.rb
+++ spec/requests/api/v1/invoices/search_request_spec.rb
@@ -86,4 +86,92 @@ describe 'Invoices API' do
       expect(invoice[:id]).to eq(2)
     end
   end
+
+  context 'GET /api/v1/invoices/find_all?parameters' do
+    it 'can find all invoices based on id' do
+      create_list(:invoice, 6)
+
+      get '/api/v1/invoices/find_all?id=3'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+      invoice = invoices.first
+
+      expect(invoices.count).to eq(1)
+      expect(invoice[:id]).to eq(3)
+      expect(invoice).to have_key(:customer_id)
+      expect(invoice).to have_key(:merchant_id)
+      expect(invoice).to have_key(:status)
+      expect(invoice).to have_key(:created_at)
+      expect(invoice).to have_key(:updated_at)
+    end
+    it 'can find all invoices based on customer_id' do
+      create_list(:customer, 10)
+      create(:invoice, customer_id: 4)
+      create(:invoice, customer_id: 2)
+      create(:invoice, customer_id: 4)
+
+      get '/api/v1/invoices/find_all?customer_id=4'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoices.count).to eq(2)
+    end
+    it 'can find all invoices based on merchant_id' do
+      create_list(:merchant, 10)
+      create(:invoice, merchant_id: 4)
+      create(:invoice, merchant_id: 2)
+      create(:invoice, merchant_id: 4)
+
+      get '/api/v1/invoices/find_all?merchant_id=4'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoices.count).to eq(2)
+    end
+    it 'can find all invoices based on status' do
+      create(:invoice, status: 'shipped')
+      create(:invoice, status: 'returned')
+      create(:invoice, status: 'returned')
+
+      get '/api/v1/invoices/find_all?status=returned'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoices.count).to eq(2)
+    end
+    it 'can find all invoices based on created_at' do
+      create(:invoice, created_at: '2012-01-12 09:54:09 UTC')
+      create(:invoice, created_at: '2012-03-25 09:54:09 UTC')
+      create(:invoice, created_at: '2012-01-12 09:54:09 UTC')
+
+      get '/api/v1/invoices/find_all?created_at=2012-01-12 09:54:09 UTC'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoices.count).to eq(2)
+    end
+    it 'can find all invoices based on updated_at' do
+      create(:invoice, updated_at: '2012-01-12 09:54:09 UTC')
+      create(:invoice, updated_at: '2012-03-25 09:54:09 UTC')
+      create(:invoice, updated_at: '2012-01-12 09:54:09 UTC')
+
+      get '/api/v1/invoices/find_all?updated_at=2012-01-12 09:54:09 UTC'
+
+      expect(response).to be_successful
+
+      invoices = JSON.parse(response.body, symbolize_names: true)
+
+      expect(invoices.count).to eq(2)
+    end
+  end
 end
