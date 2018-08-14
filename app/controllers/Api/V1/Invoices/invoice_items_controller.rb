4d11e12c85c295c39f1ef3c2fba3eb416785597d
JP Lynch <JPLynch35@gmail.com>
2018-08-14 14:09:58 -0600

    alter invoice relationship controller actions

--- app/controllers/Api/V1/Invoices/invoice_items_controller.rb
+++ app/controllers/Api/V1/Invoices/invoice_items_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Invoices::InvoiceItemsController < ApplicationController
   def index
-    render json: Invoice.includes(:invoice_items).find(params[:invoice_id]).invoice_items
-    # render json: InvoiceItem.where(invoice_id: params[:invoice_id])
+    render json: InvoiceItem.where(invoice_id: params[:invoice_id])
   end
 end
