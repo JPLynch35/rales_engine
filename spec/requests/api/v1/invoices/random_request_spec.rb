8aaf6e57c7fa4b47dd5148d2254326838305f624
JerrelMitchell <jerrelmitchell@ymail.com>
2018-08-14 14:20:29 -0600

    fix grammar error

--- spec/requests/api/v1/invoices/random_request_spec.rb
+++ spec/requests/api/v1/invoices/random_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Invoices API' do
   context 'GET /api/v1/invoices/random' do
-    it 'can a random invoice' do
+    it 'can return a random invoice' do
       create_list(:invoice, 3)
 
       get '/api/v1/invoices/random'
