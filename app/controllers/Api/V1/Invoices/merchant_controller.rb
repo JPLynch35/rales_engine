c3b49453cbb961d2fafa3142efe19c885c75157f
JP Lynch <JPLynch35@gmail.com>
2018-08-14 14:50:35 -0600

    add merchant serializer to invoices to merchant relationship endpoint

--- app/controllers/Api/V1/Invoices/merchant_controller.rb
+++ app/controllers/Api/V1/Invoices/merchant_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Invoices::MerchantController < ApplicationController
   def index
-    render json: Invoice.includes(:merchant).find(params[:invoice_id]).merchant
+    render json: Invoice.includes(:merchant).find(params[:invoice_id]).merchant, serialize: MerchantSerializer
   end
 end
