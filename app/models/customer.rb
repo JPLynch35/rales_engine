dcbf81c72a4d235e6bbbc2097ed7cb70ee4a6da7
JP Lynch <JPLynch35@gmail.com>
2018-08-15 11:28:03 -0600

    add default scope of ordering to all models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
   has_many :invoices
   has_many :merchants, through: :invoices
+
+  default_scope -> {order(id: :asc)}
 end
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,6 @@ class InvoiceItem < ApplicationRecord
   validates_presence_of :item_id, :invoice_id, :quantity, :unit_price
   belongs_to :invoice
   belongs_to :item
+
+  default_scope -> {order(id: :asc)}
 end
