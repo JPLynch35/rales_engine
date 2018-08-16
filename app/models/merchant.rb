002c24259407a4a30e4bdca395d0df3b7579fc71
JP Lynch <JPLynch35@gmail.com>
2018-08-16 10:43:52 -0600

    add business intelligence for top x merchants with most revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,14 @@ class Merchant < ApplicationRecord
   has_many :customers, through: :invoices
 
   default_scope -> {order(id: :asc)}
+
+  def self.most_revenue(num_merchants)
+    unscoped
+    .select('merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS sub_total')
+    .joins(:invoices, invoices: [:transactions, :invoice_items])
+    .merge(Transaction.unscoped.success)
+    .group(:id)
+    .order('sub_total DESC')
+    .limit(num_merchants)
+  end
 end
